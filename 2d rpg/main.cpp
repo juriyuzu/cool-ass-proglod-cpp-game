f65ed3a0b363bff3a8b5ae358678f97d6bea5f55
juriyuzu <juliusantonaquino.joules@gmail.com>
2023-10-01 14:59:25 +0800

    asdf

--- 2d rpg/main.cpp
+++ 2d rpg/main.cpp
@@ -3,15 +3,41 @@
 using namespace std;
 
 char x;
-int coords[2] = {3, 3};
+int coords[] = {1, 1, 0};
+int ray[4][4][3][2] = {
+    {
+        {{-3, -1}, {-3, 0}, {-3, 1}},
+        {{-2, -1}, {-2, 0}, {-2, 1}},
+        {{-1, -1}, {-1, 0}, {-1, 1}},
+        {{0, -1}, {0, 1}}
+    },
+    {
+        {{-1, 3}, {0, 3}, {1, 3}},
+        {{-1, 2}, {0, 2}, {1, 2}},
+        {{-1, 1}, {0, 1}, {1, 1}},
+        {{-1, 0}, {1, 0}}
+    },
+    {
+        {{3, 1}, {3, 0}, {3, -1}},
+        {{2, 1}, {2, 0}, {2, -1}},
+        {{1, 1}, {1, 0}, {1, -1}},
+        {{0, 1}, {0, -1}}
+    },
+    {
+        {{1, -3}, {0, -3}, {-1, -3}},
+        {{1, -2}, {0, -2}, {-1, -2}},
+        {{1, -1}, {0, -1}, {-1, -1}},
+        {{1, 0}, {-1, 0}}
+    }
+};
 char map[7][11] = {
-    "##########",
-    "#        #",
-    "#        #",
-    "#        #",
-    "#        #",
-    "#        #",
-    "##########"
+    "#########",
+    "#   #   #",
+    "# # # # #",
+    "# ## ## #",
+    "# #   # #",
+    "#   #   #",
+    "#########"
 };
 
 void play();
@@ -31,16 +57,16 @@ void play() {
 
         switch (x) {
             case 'w':
-                if (map[coords[0] - 1][coords[1]] == ' ') coords[0]--;
+                if (map[coords[0] - 1][coords[1]] == ' ' && coords[2] == 0) coords[0]--;
+                if (map[coords[0]][coords[1] + 1] == ' ' && coords[2] == 1) coords[1]++;
+                if (map[coords[0] + 1][coords[1]] == ' ' && coords[2] == 2) coords[0]++;
+                if (map[coords[0]][coords[1] - 1] == ' ' && coords[2] == 3) coords[1]--;
                 break;
-            case 'a':
-                if (map[coords[0]][coords[1] - 1] == ' ') coords[1]--;
+            case 'q':
+                coords[2] += (coords[2] != 0) ? -1 : 3;
                 break;
-            case 's':   
-                if (map[coords[0] + 1][coords[1]] == ' ') coords[0]++;
-                break;
-            case 'd':
-                if (map[coords[0]][coords[1] + 1] == ' ') coords[1]++;
+            case 'e':
+                coords[2] += (coords[2] != 3) ? 1 : -3;
                 break;
         }
 
@@ -60,5 +86,19 @@ void display(int y, int x) {
         }
         cout << endl;
     }
-    cout << endl;
+    cout << "Facing " << coords[2] << endl
+         << map[coords[0] + ray[coords[2]][0][0][0]][coords[1] + ray[coords[2]][0][0][1]] 
+            << map[coords[0] + ray[coords[2]][0][1][0]][coords[1] + ray[coords[2]][0][1][1]] 
+            << map[coords[0] + ray[coords[2]][0][2][0]][coords[1] + ray[coords[2]][0][2][1]] << endl
+         << map[coords[0] + ray[coords[2]][1][0][0]][coords[1] + ray[coords[2]][1][0][1]] 
+            << map[coords[0] + ray[coords[2]][1][1][0]][coords[1] + ray[coords[2]][1][1][1]] 
+            << map[coords[0] + ray[coords[2]][1][2][0]][coords[1] + ray[coords[2]][1][2][1]] << endl
+         << map[coords[0] + ray[coords[2]][2][0][0]][coords[1] + ray[coords[2]][2][0][1]] 
+            << map[coords[0] + ray[coords[2]][2][1][0]][coords[1] + ray[coords[2]][2][1][1]] 
+            << map[coords[0] + ray[coords[2]][2][2][0]][coords[1] + ray[coords[2]][2][2][1]] << endl
+         << map[coords[0] + ray[coords[2]][3][0][0]][coords[1] + ray[coords[2]][3][0][1]] 
+            << '@' 
+            << map[coords[0] + ray[coords[2]][3][1][0]][coords[1] + ray[coords[2]][3][1][1]] << endl
+         << endl;
+
 }
